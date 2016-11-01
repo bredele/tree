43edaa4bebb13abcf472a8d6043d6577a17fa9a6
bredele <olivier.wietrich@gmail.com>
2016-11-01 13:07:20 -0600

    added test environment

---
+++ test/tree.js
@@ -0,0 +1,24 @@
+/**
+ * Test dependencies.
+ */
+
+var test = require('tape')
+var tree = require('..')
+
+test('should convert an array into a tree', assert => {
+  assert.plan(1)
+  assert.deepEqual(['root', 'male', 'canada'], {
+    name: 'root',
+    children : [
+      {
+        name: 'male',
+        children : [
+          {
+            name: 'canada',
+            children: []
+          }
+        ]
+      }
+    ]
+  })
+})
